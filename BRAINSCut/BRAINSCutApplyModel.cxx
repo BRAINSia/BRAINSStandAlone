859238c24f2e34d700f7ccb13a6848b0c8e6e593
Regina Kim <eunyoung-kim@uiowa.edu>
2011-11-21 12:44:43 -0600

    ENH: write deformed ROI

--- BRAINSCut/BRAINSCutApplyModel.cxx
+++ BRAINSCut/BRAINSCutApplyModel.cxx
@@ -88,6 +88,7 @@ BRAINSCutApplyModel
                                                          imagesOfInterest.front(),
                                                          deformedROIs.find( *roiTyIt )->second,
                                                          ANNContinuousOutputFilename );
+
         /* post processing
          * may include hole-filling(closing), thresholding, and more adjustment
          */
@@ -96,6 +97,8 @@ BRAINSCutApplyModel
 
         std::string roiOutputFilename = GetROIVolumeName( subject, *roiTyIt );
         itkUtil::WriteImage<BinaryImageType>( mask, roiOutputFilename );
+        itkUtil::WriteImage<WorkingImageType>( deformedROIs.find( *roiTyIt )->second,
+                                               roiOutputFilename+"def.nii.gz");
         }
       else /* testing phase */
         {
@@ -273,7 +276,6 @@ BRAINSCutApplyModel
   closingFilter->SetSafeBorder( true );
   closingFilter->Update();
 
-  itkUtil::WriteImage<BinaryImageType>(  closingFilter->GetOutput(), "DEBUGCloseed.nii.gz");
   return closingFilter->GetOutput();
 }
 inline void
