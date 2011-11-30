581cf241247ad20b53194155de905e1dccb0bc22
Mark Scully <mark-scully@uiowa.edu>
2011-11-30 14:23:20 -0600

    Working version of thresholded labels. Some things still hardcoded.

--- BRAINSABC/brainseg/BRAINSABC.cxx
+++ BRAINSABC/brainseg/BRAINSABC.cxx
@@ -1522,6 +1522,13 @@ int main(int argc, char * *argv)
     writer->SetInput( segfilter->GetCleanedOutput() );
     writer->SetFileName( fn.c_str() );
     writer->UseCompressionOn();
+
+    fn = outputDir;
+    fn += "thresholded_labels.nii.gz";
+    writer->SetInput( segfilter->GetThresholdedOutput() );
+    writer->SetFileName( fn );
+    writer->UseCompressionOn();
+
     try
       {
       writer->Update();
