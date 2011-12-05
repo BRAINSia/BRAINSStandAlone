c68ac373f8e38bc565f1827c73cbec91177cf0cb
Regina Kim <eunyoung-kim@uiowa.edu>
2011-12-05 09:32:04 -0600

    ENH: Get Rid of Old src files

--- BRAINSCut/BRAINSCutGenerateRegistrations.h
+++ BRAINSCut/BRAINSCutGenerateRegistrations.h
@@ -24,6 +24,18 @@ private:
 
   std::list<DataSet *> subjectDataSets;
 
+  /** private functions */
+
+  void  CreateTransformFile(const std::string & MovingImageFilename,
+                            const std::string & FixedImageFilename,
+                            const std::string & MovingBinaryImageFilename,
+                            const std::string & FixedBinaryImageFilename,
+                            const std::string & OutputRegName,
+                            bool verbose);
+
+
+
+
 };
 
 #endif
