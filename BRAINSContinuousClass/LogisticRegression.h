5e6d3eec1df0e311a117390f2001ba5d6731cf97
Mark Scully <mark-scully@uiowa.edu>
2012-01-23 11:15:58 -0600

    (Usable) Fixed logic bugs and added some error checking.

--- BRAINSContinuousClass/LogisticRegression.h
+++ BRAINSContinuousClass/LogisticRegression.h
@@ -11,17 +11,19 @@ class LogisticRegressionSample {
 private:
   std::vector<TSampleType> *m_sample;
   unsigned int m_label;
-  std::map<unsigned int, double> m_predictedProbability[2];
-  bool m_ReversedClassOrder;
+  std::map<unsigned int, double> m_predictedProbability;
+  bool m_labelSet;
 
 public:
   LogisticRegressionSample(const unsigned int featureCount);
   ~LogisticRegressionSample();
-  double const GetLabelProbability(unsigned int const &) const;
-  void SetSample(std::vector<TSampleType> *);
+  double GetLabelProbability(unsigned int const &);
+  void SetSample(std::vector<TSampleType> &);
   std::vector<TSampleType> const * GetSample() const;
   void SetLabelProbability(unsigned int const &, double const &);
   unsigned int const GetLabel() const {return this->m_label;};
+  void SetLabel(unsigned int const &) ;
+  bool LabelIsSet() const {return this->m_labelSet;};
 
 };
 
@@ -38,6 +40,8 @@ private:
   unsigned int m_featureCount;
   unsigned int m_classOneLabel;
   unsigned int m_classTwoLabel;
+  bool m_classOneLabelSet;
+  bool m_classTwoLabelSet;
 
 public:
   LogisticRegression (const unsigned int featureCount, const unsigned int sampleCount);
@@ -48,4 +52,5 @@ public:
   void SetClassTwoLabel(const unsigned int);
   void ClassifySample(LogisticRegressionSample<TSampleType> &);
 };
+#include "LogisticRegression.hxx"
 #endif
