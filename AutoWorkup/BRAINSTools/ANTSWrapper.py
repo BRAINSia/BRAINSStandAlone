6d22e5052a1eada449801d02f874c1bb2c80d2b0
Hans Johnson <hans-johnson@uiowa.edu>
2012-04-08 07:09:36 -0500

    ENH: Updated to use the ITKv4 compliant antsRegistration tool.

--- AutoWorkup/BRAINSTools/ANTSWrapper.py
+++ AutoWorkup/BRAINSTools/ANTSWrapper.py
@@ -21,45 +21,14 @@ $ ANTS_wrapper.sh <FixedT1Image> <FixedT2Image> <MovingT1Image> <MovingT2Image> 
 """
 from nipype.interfaces.base import ( File, TraitedSpec, Interface, CommandLineInputSpec, CommandLine, traits )
 
-### Node Interface
-class ANTSWrapperInputSpec( TraitedSpec ):
-    fixed_T1_image = File( desc = "T1 Fixed Volume", exists = True, mandatory = True)
-    fixed_T2_image = File( desc = "T2 Fixed Volume", exists = True, mandatory = True)
-    moving_T1_image = File( desc = "T1 Moving Volume", exists = True, mandatory = True)
-    moving_T2_image = File( desc = "T2 Moving Volume", exists = True, mandatory = True)
-    prefix_desc = "Output volume prefix for Affine.txt, Warp.nii.gz, and InverseWarp.nii.gz"
-    output_prefix = traits.Str( desc = prefix_desc, exists = True, mandatory = False,
-                                default_value = "./ANTS_")
-
-class ANTSWrapperOutputSpec(TraitedSpec):
-    output_affine = File( desc = "Affine Transform Text File")
-    output_warp = File( desc = "Warped Output Volume")
-    output_invwarp = File( desc = "Inverse Warp Output Volume")
-
-class ANTSWrapper(Interface):
-    input_spec = ANTSWrapperInputSpec
-    output_spec = ANTSWrapperOutputSpec
-
-    def _list_outputs(self):
-        """
-        The ANTS package implicitly outputs three files based on the prefix given at the commandline:
-        {prefix}Affine.txt, {prefix}Warp.nii.gz, and {prefix}InverseWarp.nii.gz.  This function exposes
-        them for subsequent nodes.
-        """
-        from os.path import abspath
-        outputs = self.output_spec().get()
-        outputs['output_affine'] = abspath( self.inputs.output_prefix + 'Affine.txt')
-        outputs['output_warp'] = abspath( self.inputs.output_prefix + 'Warp.nii.gz')
-        outputs['output_invwarp'] = abspath( self.inputs.output_prefix + 'InverseWarp.nii.gz')
-        return outputs
-
 ### CommandLine
 class ANTSWrapperCLInputSpec(CommandLineInputSpec):
     fixed_T1_image = File(desc="T1 Fixed Volume", exists=True, mandatory=True, position=0, argstr="%s" )
     fixed_T2_image = File(desc="T2 Fixed Volume", exists=True, mandatory=True, position=1, argstr="%s" )
     moving_T1_image = File(desc="T1 Moving Volume", exists=True, mandatory=True, position=2, argstr="%s" )
     moving_T2_image = File(desc="T2 Moving Volume", exists=True, mandatory=True, position=3, argstr="%s" )
-    prefix_desc = "Output volume prefix for Affine.txt, Warp.nii.gz, and InverseWarp.nii.gz"
+    initialTransform = File( desc = "Initial Transform", exists = True, mandatory = True, position=5, argstr="%s" )
+    prefix_desc = "Output volume prefix for Affine.txt, _1Warp.nii.gz, and _1InverseWarp.nii.gz"
     output_prefix = traits.Str(desc=prefix_desc, exists=True, mandatory=False, position=4, argstr="%s",
                                default_value="./ANTS_" )
 
@@ -76,14 +45,14 @@ class ANTSWrapper(CommandLine):
     def _list_outputs(self):
         """
         The ANTS package implicitly outputs three files based on the prefix given at the commandline:
-        {prefix}Affine.txt, {prefix}Warp.nii.gz, and {prefix}InverseWarp.nii.gz.  This function exposes
+        {prefix}Affine.txt, {prefix}_{index}Warp.nii.gz, and {prefix}_{index}InverseWarp.nii.gz.  This function exposes
         them for subsequent nodes.
         """
         from os.path import abspath
         outputs = self.output_spec().get()
         outputs['output_affine'] = abspath( self.inputs.output_prefix + 'Affine.txt')
-        outputs['output_warp'] = abspath( self.inputs.output_prefix + 'Warp.nii.gz')
-        outputs['output_invwarp'] = abspath( self.inputs.output_prefix + 'InverseWarp.nii.gz')
+        outputs['output_warp'] = abspath( self.inputs.output_prefix + '_1' + 'Warp.nii.gz')
+        outputs['output_invwarp'] = abspath( self.inputs.output_prefix + '_1' + 'InverseWarp.nii.gz')
         return outputs
 
 if __name__ == '__main__':
--- AutoWorkup/BRAINSTools/ANTS_wrapper.sh
+++ AutoWorkup/BRAINSTools/ANTS_wrapper.sh
@@ -13,6 +13,7 @@ FIXED_T2=$2
 MOVING_T1=$3
 MOVING_T2=$4
 OUT_PREFIX=$5
+AffineRegistrationInput=$6
 
 #OUTPUT_AFFINE=$5
 #OUTPUT_DEFORMATION=$6
@@ -33,7 +34,7 @@ export DIM=3
 #   --MI-option 32x16000
 #fi
 
-if [ 1 -eq 1 ] ;then
+if [ 0 -eq 1 ] ;then
 ${ANTSPATH}/ANTS ${DIM}  \
    -m  CC[${FIXED_T1},${MOVING_T1},1,5]  \
    -m  CC[${FIXED_T2},${MOVING_T2},1,5] \
@@ -44,6 +45,31 @@ ${ANTSPATH}/ANTS ${DIM}  \
    --use-Histogram-Matching  \
    --number-of-affine-iterations 10000x10000x10000x10000x10000 \
    --MI-option 32x16000
+else
+   echo antsRegistration \
+   -d 3  \
+   -m CC[${FIXED_T1},${MOVING_T1},1,5]  \
+   -m CC[${FIXED_T2},${MOVING_T2},1,5] \
+   -t SyN[0.25,3.0,0.0] \
+   -r [${AffineRegistrationInput},0] \
+   -o ${OUT_PREFIX} \
+   -c [70x70x20,1e-6,10] \
+   -f 3x2x1 \
+   -s 0x0x0 \
+   -u 1
+
+#MxNxO,<convergenceThreshold=1e-6>,<convergenceWindowSize=10
+   antsRegistration \
+   -d 3  \
+   -m CC[${FIXED_T1},${MOVING_T1},1,5]  \
+   -m CC[${FIXED_T2},${MOVING_T2},1,5] \
+   -t SyN[0.25,3.0,0.0] \
+   -r [${AffineRegistrationInput},0] \
+   -o ${OUT_PREFIX} \
+   -c [70x70x20,1e-6,10] \
+   -f 3x2x1 \
+   -s 0x0x0 \
+   -u 1
 fi
 
 #  ${ANTSPATH}/ANTS \
