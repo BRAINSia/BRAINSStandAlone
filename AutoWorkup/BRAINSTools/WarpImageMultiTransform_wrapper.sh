f7fa6fe9bf2a896545182fc9dadd6970d77245fe
Hans Johnson <hans-johnson@uiowa.edu>
2012-04-13 08:27:50 -0500

    ENH:  Updates to the pipeline for HPC use
    Many updates to fine tune the processing of the workflow
    on HPC cluster running SGE.  Some of the resource heavy
    sections are now explicitly given extra resources.

--- AutoWorkup/BRAINSTools/WarpImageMultiTransform_wrapper.sh
+++ AutoWorkup/BRAINSTools/WarpImageMultiTransform_wrapper.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-if [ -d /scratch/PREDICT/johnsonhj/src/ANTS-gcc44/Examples ] ;then
-  export ANTSPATH=/scratch/PREDICT/johnsonhj/src/ANTS-gcc44/Examples/
-fi
-if [ -d /ipldev/scratch/johnsonhj/src/ANTS-Darwin-build/Examples ];then
-  export ANTSPATH=/ipldev/scratch/johnsonhj/src/ANTS-Darwin-build/Examples
-fi
-
 AFFINE_TRANSFORM=$1
 DEFORMATION_FIELD=$2
 REFERENCE_IMAGE=$3
 MOVING_IMAGE=$4
 OUTPUT_IMAGE=$5
 
-#OUTPUT_AFFINE=$5
-#OUTPUT_DEFORMATION=$6
-
-
 export DIM=3
-
-${ANTSPATH}/WarpImageMultiTransform \
+WarpImageMultiTransform \
     $DIM \
     ${MOVING_IMAGE} \
     ${OUTPUT_IMAGE} \
