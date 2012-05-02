fdded6974d0a7a9b95dd6e96345dd6150d4175c7
David Welch <david.m.welch@gmail.com>
2012-05-02 15:32:33 -0500

    ENH: Completed antsRegistration.py for Nipype

--- AutoWorkup/BRAINSTools/ants/antsRegistration.py
+++ AutoWorkup/BRAINSTools/ants/antsRegistration.py
@@ -184,34 +184,18 @@ class AntsRegistrationInputSpec(ANTSCommandInputSpec):
     n_iterations = traits.List(traits.Int(), argstr="%s")
     convergence_threshold = traits.Float(requires=['n_iterations'])
     convergence_window_size = traits.Int(requires=['n_iterations', 'convergence_threshold'])
-    # masks = ???
-    # out_postfix = traits.Str('_wimt', argstr='%s', usedefault=True,
-    #                          desc=('Postfix that is prepended to all output '
-    #                                'files (default = _wimt)'))
-    # reference_image = File(argstr='-R %s', xor=['tightest_box'],
-    #                    desc='reference image space that you wish to warp INTO')
-    # tightest_box = traits.Bool(argstr='--tightest-bounding-box',
-    #                       desc=('computes tightest bounding box (overrided by '  \
-    #                             'reference_image if given)'),
-    #                       xor=['reference_image'])
-    # reslice_by_header = traits.Bool(argstr='--reslice-by-header',
-    #                  desc=('Uses orientation matrix and origin encoded in '
-    #                        'reference image file header. Not typically used '
-    #                        'with additional transforms'))
-    # use_nearest = traits.Bool(argstr='--use-NN',
-    #                           desc='Use nearest neighbor interpolation')
-    # use_bspline = traits.Bool(argstr='--use-Bspline',
-    #                           desc='Use 3rd order B-Spline interpolation')
-    # transformation_series = InputMultiPath(File(exists=True), argstr='%s',
-    #                          desc='transformation file(s) to be applied',
-    #                          mandatory=True, copyfile=False)
-    # invert_affine = traits.List(traits.Int,
-    #                 desc=('List of Affine transformations to invert. '
-    #                       'E.g.: [1,4,5] inverts the 1st, 4th, and 5th Affines '
-    #                       'found in transformation_series'))
+    shrink_factors = traits.List(traits.Int(), argstr="--shrink-factors %s", sep='x')
+    smoothing_sigmas = traits.List(traits.Int(), argstr="--smoothing-sigmas %s", sep='x')
+    use_histogram_matching = traits.Bool(argstr="--use-histogram-matching")
+    output_warped_image = traits.Either(traits.Bool, File(), hash_files=False, desc="")
+    output_inverted_warped_image = traits.Either(traits.Bool, File(), hash_files=False, requires=['output_warped_image'], desc="")
+    output_transform_prefix = traits.Str("transform", usedefault=True, argstr="%s", desc="")
 
 class AntsRegistrationOutputSpec(TraitedSpec):
-    output_image = File(exists=True, desc='Warped image')
+    warped_image = File(exists=True, desc='Warped image')
+    inverted_warped_image = File(exists=True, desc='Inverted warped image')
+    warped_transform = File(exists=True, desc='Warp transform')
+    inverted_warped_transform = File(exists=True, desc='Inverted warp transform')
 
 class AntsRegistration(ANTSCommand):
     """
@@ -253,81 +237,36 @@ class AntsRegistration(ANTSCommand):
                                                    self.inputs.convergence_threshold)
             else:
                 return "--convergence %s"%(convergence_iter)
-                
+        elif opt == 'output_transform_prefix':
+            if isdefined(self.inputs.output_inverted_warped_image) and self.inputs.output_inverted_warped_image:
+                return '--output [%s, %s, %s]' %(self.inputs.output_transform_prefix, self._getOutputWarpedImageFileName(), self._getOutputWarpedImageFileName(inverted=True))
+            elif isdefined(self.inputs.output_warped_image) and self.inputs.output_warped_image:
+                return '--output [%s, %s]' %(self.inputs.output_transform_prefix, self._getOutputWarpedImageFileName())
+            else:
+                return '--output %s' % self.inputs.output_transform_prefix
+
         return super(AntsRegistration, self)._format_arg(opt, spec, val)
+
+    def _getOutputWarpedImageFileName(self, inverted=False):
+        value = self.inputs.output_warped_image
+        _, fixedName, ext = split_filename(self.inputs.fixed_image[0])
+        _, movingName, __ = split_filename(self.inputs.moving_image[0])
+        if inverted:
+            defaultName = '%s_to_%s%s' % (fixedName, movingName, ext)
+        else:
+            defaultName = '%s_to_%s%s' % (movingName, fixedName, ext)
+        if isinstance(value, bool):
+            if value == True:
+                return defaultName
+        else:
+            return value
 
     def _list_outputs(self):
         outputs = self._outputs().get()
-        _, name, ext = split_filename(os.path.abspath(self.inputs.moving_image))
-        outputs['output_image'] = os.path.join(os.getcwd(),
-                                             ''.join((name,
-                                                      self.inputs.out_postfix,
-                                                      ext)))
+        outputs['warped_transform'] = os.path.abspath(self.inputs.output_transform_prefix + '1Warp.nii.gz')
+        outputs['inverted_warped_transform'] = os.path.abspath(self.inputs.output_transform_prefix + '1InverseWarp.nii.gz')
+        if isdefined(self.inputs.output_warped_image) and self.inputs.output_warped_image:
+            outputs['warped_image'] = os.path.abspath(self._getOutputWarpedImageFileName())
+        if isdefined(self.inputs.output_inverted_warped_image) and self.inputs.output_inverted_warped_image:
+            outputs['inverted_warped_image'] = os.path.abspath(self._getOutputWarpedImageFileName(inverted=True))
         return outputs
-
-"""
-  antsRegistration \
-        -d 3 \
-        -m CC[/IPLlinux/ipldev/scratch/johnsonhj/PREDICT/ExpandedExperiment/B4AUTO.ExpandedExperiment/BAW_20120104_workflow/_uid_PHD_024_0003_42245/11_BABC/t1_average_BRAINSABC.nii.gz,/ipldev/scratch/johnsonhj/src/BRAINSStandAlone-Darwin-clang/ReferenceAtlas-build/Atlas/Atlas_20120104/template_t1.nii.gz,1,5] \
-        -m CC[/IPLlinux/ipldev/scratch/johnsonhj/PREDICT/ExpandedExperiment/B4AUTO.ExpandedExperiment/BAW_20120104_workflow/_uid_PHD_024_0003_42245/11_BABC/t2_average_BRAINSABC.nii.gz,/ipldev/scratch/johnsonhj/src/BRAINSStandAlone-Darwin-clang/ReferenceAtlas-build/Atlas/Atlas_20120104/template_t2.nii.gz,1,5] \
-        -t SyN[0.25,3.0,0.0] \
-        -r [/IPLlinux/ipldev/scratch/johnsonhj/PREDICT/ExpandedExperiment/B4AUTO.ExpandedExperiment/BAW_20120104_workflow/_uid_PHD_024_0003_42245/05_BLI/landmarkInitializer_atlas_to_subject_transform.mat,0] \
-        -o ANTS_ \
-        -c [70x70x20,1e-6,10] \
-        -f 3x2x1 \
-        -s 0x0x0 \
-        -u 1
-"""
-
-class antsRegistrationInputSpec(ANTSCommandInputSpec):
-    dimension = traits.Enum(3, 2, argstr='-d %d', usedefault=True,
-                             desc='image dimension (2 or 3)', position=1)
-    reference_image = File(exists=True,
-                           argstr='-r %s', desc='template file to warp to',
-                           mandatory=True, copyfile=True)
-    input_image = File(exists=True,
-                       argstr='-i %s', desc='input image to warp to template',
-                       mandatory=True, copyfile=False)
-    force_proceed = traits.Bool(argstr='-f 1',
-                             desc=('force script to proceed even if headers '
-                                   'may be incompatible'))
-    inverse_warp_template_labels = traits.Bool(argstr='-l',
-                       desc=('Applies inverse warp to the template labels '
-                             'to estimate label positions in target space (use '
-                             'for template-based segmentation)'))
-    max_iterations = traits.List(traits.Int, argstr='-m %s', sep='x',
-                             desc=('maximum number of iterations (must be '
-                                   'list of integers in the form [J,K,L...]: '
-                                   'J = coarsest resolution iterations, K = '
-                                   'middle resolution interations, L = fine '
-                                   'resolution iterations'))
-    bias_field_correction = traits.Bool(argstr='-n 1',
-                                desc=('Applies bias field correction to moving '
-                                      'image'))
-    similarity_metric = traits.Enum('PR', 'CC', 'MI', 'MSQ', argstr='-s %s',
-            desc=('Type of similartiy metric used for registration '
-                  '(CC = cross correlation, MI = mutual information, '
-                  'PR = probability mapping, MSQ = mean square difference)'))
-    transformation_model = traits.Enum('GR', 'EL', 'SY', 'S2', 'EX', 'DD', 'RI',
-                                       'RA', argstr='-t %s', usedefault=True,
-               desc=('Type of transofmration model used for registration '
-                     '(EL = elastic transformation model, SY = SyN with time, '
-                     'arbitrary number of time points, S2 =  SyN with time '
-                     'optimized for 2 time points, GR = greedy SyN, EX = '
-                     'exponential, DD = diffeomorphic demons style exponential '
-                     'mapping, RI = purely rigid, RA = affine rigid'))
-    out_prefix = traits.Str('ants_', argstr='-o %s', usedefault=True,
-                             desc=('Prefix that is prepended to all output '
-                                   'files (default = ants_)'))
-    quality_check = traits.Bool(argstr='-q 1',
-                             desc='Perform a quality check of the result')
-
-
-class antsRegistrationOutputSpec(TraitedSpec):
-    affine_transformation = File(exists=True, desc='affine (prefix_Affine.txt)')
-    warp_field = File(exists=True, desc='warp field (prefix_Warp.nii)')
-    inverse_warp_field = File(exists=True,
-                            desc='inverse warp field (prefix_InverseWarp.nii)')
-    input_file = File(exists=True, desc='input image (prefix_repaired.nii)')
-    output_file = File(exists=True, desc='output image (prefix_deformed.nii)')
-
--- AutoWorkup/BRAINSTools/ants/example.py
+++ AutoWorkup/BRAINSTools/ants/example.py
@@ -1,3 +1,7 @@
+# from nipype import config, logging
+# config.enable_debug_mode()
+# logging.update_logging(config)
+
 import antsRegistration
 
 test = antsRegistration.AntsRegistration()
@@ -11,15 +15,17 @@ test.inputs.transform = "SyN[0.25,3.0,0.0]"
 test.inputs.n_iterations = [100, 70, 20]
 test.inputs.convergence_threshold = 1e-6
 test.inputs.convergence_window_size = 10
-# test.inputs.fixed_image_masks =
+test.inputs.shrink_factors = [3,2,1]
+test.inputs.smoothing_sigmas = [0,0,0]
+test.inputs.use_histogram_matching = True
+test.inputs.output_warped_image = True
+test.inputs.output_inverted_warped_image = True
 
-target = "antsRegistration --dimensionality 3 --initial-moving-transform 20120430_1348_txfmv2fv_affine.mat --metric 'CC[{0}SUBJ_A_T1_resampled.nii.gz,{0}SUBJ_B_T1_resampled.nii.gz,1,5]' --masks [SUBJ_A_small_T2_mask.nii.gz,SUBJ_B_small_T2_mask.nii.gz] --convergence [100x70x20,1e-06,10] --transform SyN[0.25,3.0,0.0]".format('/hjohnson/HDNI/EXPERIEMENTS/ANTS_NIPYPE_SMALL_TEST/')
 
-# -t 'SyN[0.25,3.0,0.0]' \
-#     -c '[100x70x20,1e-6,10]' \
-#     -f 3x2x1 \
-#     -s 0x0x0 \
-#     -u 1'
+result = test.run()
+print result.outputs
+
+target = "antsRegistration --dimensionality 3 --initial-moving-transform 20120430_1348_txfmv2fv_affine.mat --metric 'CC[{0}SUBJ_A_T1_resampled.nii.gz,{0}SUBJ_B_T1_resampled.nii.gz,1,5]' --masks [SUBJ_A_small_T2_mask.nii.gz,SUBJ_B_small_T2_mask.nii.gz] --convergence [100x70x20,1e-06,10] --shrink-factors 3x2x1 --smoothing-sigmas 0x0x0 --transform SyN[0.25,3.0,0.0]".format('/hjohnson/HDNI/EXPERIEMENTS/ANTS_NIPYPE_SMALL_TEST/')
 
 print test.cmdline
 print '++++++++++++++++'
