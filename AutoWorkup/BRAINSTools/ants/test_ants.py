028c3e857524fee837892d57feac21917c7674c6
David Welch <david.m.welch@gmail.com>
2012-05-03 11:28:38 -0500

    SQSH: Finished ants.py Nipype interface proposal and final test case in example.py
    SQSH: Added dimension input to ants.py
    
    SQSH: (ENH) Passes test for image_metric with length of one
    
    SQSH: (ENH) Passes test for image_metric with length of one
    
    SQSH: (ENH) Passes test for transform-model for SyN before Affine ONLY.  Need to generalize this for different models and numbers of modifying parameters
    
    SQSH: (ENH) Passes test for number_of_iterations
    
    SQSH: (ENH) Passing for all test inputs compared to generated command line
    
    SQSH: (STYLE) _transformation_constructor code is cleaner. (BUG) Fixed --transform-model flag to correct --transformation-model flag. (ENH) Began work on output parameters
    
    SQSH: (BUG) Removed List property from transformation inputs and now have a bug in the --affine-gradient-descent-option constructor
    
    SQSH: (ENH) Finished --affine-gradient-descent-option input and has passed command line formatting testing.
    
    SQSH: (ENH) Testing outputs is incomplete, copied example.py to new test_ants.py to conform with Hans test files, lowered testing parameters for faster (hopefully) output file generation

--- AutoWorkup/BRAINSTools/ants/test_ants.py
+++ AutoWorkup/BRAINSTools/ants/test_ants.py
@@ -1,6 +1,7 @@
 # from nipype import config, logging
 # config.enable_debug_mode()
 # logging.update_logging(config)
+
 import ants
 
 testDir = '/hjohnson/HDNI/EXPERIEMENTS/ANTS_NIPYPE_SMALL_TEST'
@@ -17,9 +18,9 @@ test.inputs.metric_weight = [1.0]
 test.inputs.radius = [5]
 test.inputs.affine_gradient_descent_option = [0.25]
 test.inputs.transformation_model = 'SyN'
-test.inputs.transformation_gradient_step_length = 0.25
-test.inputs.transformation_number_of_time_steps = 3.0
-test.inputs.transformation_delta_time = 0.0
+test.inputs.gradient_step_length = 0.25
+test.inputs.number_of_time_steps = 3.0
+test.inputs.delta_time = 0.0
 test.inputs.number_of_iterations = [1,1,1]
 test.inputs.subsampling_factors = [3,2,1]
 test.inputs.gaussian_smoothing_sigmas = [0,0,0]
@@ -28,20 +29,20 @@ test.inputs.use_histogram_matching = False
 result = test.run()
 print result.outputs
 
-# target = 'ANTS \
-# 3 \
-# --affine-gradient-descent-option 0.25x0.5x0.0001x0.0001 \
-# --gaussian-smoothing-sigmas 0x0x0 \
-# --image-metric CC[{0}/SUBJ_A_T1_resampled.nii.gz,{0}/SUBJ_B_T1_resampled.nii.gz,1,5] \
-# --number-of-iterations 100x70x20 \
-# --output-naming OrigANTS_20120430_1348_ANTS6_ \
-# --subsampling-factors 3x2x1 \
-# --transformation-model SyN[0.25,3.0,0.0] \
-# --use-Histogram-Matching 1 \
-# '.format(testDir)
+target = 'ANTS \
+3 \
+--affine-gradient-descent-option 0.25x0.5x0.0001x0.0001 \
+--gaussian-smoothing-sigmas 0x0x0 \
+--image-metric CC[{0}/SUBJ_A_T1_resampled.nii.gz,{0}/SUBJ_B_T1_resampled.nii.gz,1,5] \
+--number-of-iterations 100x70x20 \
+--output-naming OrigANTS_20120430_1348_ANTS6_ \
+--subsampling-factors 3x2x1 \
+--transformation-model SyN[0.25,3.0,0.0] \
+--use-Histogram-Matching 1 \
+'.format(testDir)
 
-# print test.cmdline
-# print '++++++++++++++++'
-# print target
-# print '++++++++++++++++'
-# assert test.cmdline.strip() == target.strip()
+print test.cmdline
+print '++++++++++++++++'
+print target
+print '++++++++++++++++'
+assert test.cmdline.strip() == target.strip()
