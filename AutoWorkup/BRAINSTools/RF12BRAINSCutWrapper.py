fb30068a0d831979ca3adb027e59c9bc2f8d13a9
eunyoung kim <eunyoung.regina.kim@gmail.com>
2012-05-30 10:33:01 -0500

    ENH: BCut Interface implementation
    This is not a working copy of the script

--- AutoWorkup/BRAINSTools/RF12BRAINSCutWrapper.py
+++ AutoWorkup/BRAINSTools/RF12BRAINSCutWrapper.py
@@ -14,38 +14,61 @@ from nipype.interfaces.base import ( File, TraitedSpec, Interface, CommandLineIn
 
 ### CommandLine
 class RF12BRAINSCutWrapperCLInputSpec(CommandLineInputSpec):
-    fixed_T1_image = File(desc="T1 Fixed Volume", exists=True, mandatory=True, position=0, argstr="%s" )
-    fixed_T2_image = File(desc="T2 Fixed Volume", exists=True, mandatory=True, position=1, argstr="%s" )
-    moving_T1_image = File(desc="T1 Moving Volume", exists=True, mandatory=True, position=2, argstr="%s" )
-    moving_T2_image = File(desc="T2 Moving Volume", exists=True, mandatory=True, position=3, argstr="%s" )
-    initialTransform = File( desc = "Initial Transform", exists = True, mandatory = True, position=5, argstr="%s" )
-    prefix_desc = "Output volume prefix for Affine.txt, _1Warp.nii.gz, and _1InverseWarp.nii.gz"
-    output_prefix = traits.Str(desc=prefix_desc, exists=True, mandatory=False, position=4, argstr="%s",
-                               default_value="./ANTS_" )
+    ### subject specific
+    subjectT1Filename = File( desc="Subject T1 Volume", exists=True, mandatory=True, rgstr="--inputSubjectT1Filename %s")
+    subjectT2Filename = File( desc="Subject T2 Volume", exists=True, mandatory=True, rgstr="--inputSubjectT2Filename %s")
+    subjectSGFilename = File( desc="Subject SG Volume", exists=True, mandatory=True, rgstr="--inputSubjectSGFilename %s")
+    
+    ### model specific
+    modelFilename = File( desc="modelFilename", exists=True, mandatory=True, rgstr="--modelFilename %s")
+    trainingVectorFilename = File( desc="training vectof file name", exists=False, mandatory=False, rgstr="--trainingVectorFilename %s")
+
+    ### probability maps
+    probabilityMapsLeftAccumben = File( desc="Spatial probability map of left accumben", exists=True, mandatory=True, rgstr="--probabilityMapsLeftAccumben %s")
+    probabilityMapsRightAccumben = File( desc="Spatial probability map of right accumben", exists=True, mandatory=True, rgstr="--probabilityMapsRightAccumben %s")
+
+    probabilityMapsLeftCaudate = File( desc="Spatial probability map of left caudate", exists=True, mandatory=True, rgstr="--probabilityMapsLeftCaudate %s")
+    probabilityMapsRightCaudate = File( desc="Spatial probability map of right caudate", exists=True, mandatory=True, rgstr="--probabilityMapsRightCaudate %s")
+
+    probabilityMapsLeftGlobus = File( desc="Spatial probability map of left globus", exists=True, mandatory=True, rgstr="--probabilityMapsLeftGlobus %s")
+    probabilityMapsRightGlobus = File( desc="Spatial probability map of right globus", exists=True, mandatory=True, rgstr="--probabilityMapsRightGlobus %s")
+
+    probabilityMapsLeftHippocampus = File( desc="Spatial probability map of left hippocampus", exists=True, mandatory=True, rgstr="--probabilityMapsLeftHippocampus %s")
+    probabilityMapsRightHippocampus = File( desc="Spatial probability map of right hippocampus", exists=True, mandatory=True, rgstr="--probabilityMapsRightHippocampus %s")
+
+    probabilityMapsLeftPutamen = File( desc="Spatial probability map of left putamen", exists=True, mandatory=True, rgstr="--probabilityMapsLeftPutamen %s")
+    probabilityMapsRightPutamen = File( desc="Spatial probability map of right putamen", exists=True, mandatory=True, rgstr="--probabilityMapsRightPutamen %s")
+
+    probabilityMapsLeftThalamus = File( desc="Spatial probability map of left thalamus", exists=True, mandatory=True, rgstr="--probabilityMapsLeftThalamus %s")
+    probabilityMapsRightThalamus = File( desc="Spatial probability map of right thalamus", exists=True, mandatory=True, rgstr="--probabilityMapsRightThalamus %s")
+
 
 class RF12BRAINSCutWrapperCLOutputSpec(CommandLineInputSpec):
-    output_affine = File( desc = "Affine Transform Text File", exists = True, mandatory = False)
-    output_warp = File( desc = "Warped Output Volume", exists = True, mandatory = True )
-    output_inversewarp = File( desc = "Inverse Warp Output Volume", exists = True, mandatory = False)
+    xmlFilename = File( desc = "Net configuration xml file", exists = True, mandatory = True)
+
+    binaryLeftAccumben = File( desc = "Output binary file of left accumben", exists = True, mandatory = True)
+    binaryRightAccumben = File( desc = "Output binary file of right accumben", exists = True, mandatory = True)
+
+    binaryLeftCaudate = File( desc = "Output binary file of left caudate", exists = True, mandatory = True)
+    binaryRightCaudate = File( desc = "Output binary file of right caudate", exists = True, mandatory = True)
+
+    binaryLeftGlobus = File( desc = "Output binary file of left globus", exists = True, mandatory = True)
+    binaryRightGlobus = File( desc = "Output binary file of right globus", exists = True, mandatory = True)
+
+    binaryLeftHippocampus = File( desc = "Output binary file of left hippocampus", exists = True, mandatory = True)
+    binaryRightHippocampus = File( desc = "Output binary file of right hippocampus", exists = True, mandatory = True)
+
+    binaryLeftPutamen = File( desc = "Output binary file of left putamen", exists = True, mandatory = True)
+    binaryRightPutamen = File( desc = "Output binary file of right putamen", exists = True, mandatory = True)
+
+    binaryLeftThalamus = File( desc = "Output binary file of left thalamus", exists = True, mandatory = True)
+    binaryRightThalamus = File( desc = "Output binary file of right thalamus", exists = True, mandatory = True)
+
 
 class RF12BRAINSCutWrapper(CommandLine):
-    _cmd = 'ANTS_wrapper.sh'
+    _cmd = 'BRAINSCutCMD.sh'
     input_spec = RF12BRAINSCutWrapperCLInputSpec
     output_spec = RF12BRAINSCutWrapperCLOutputSpec
-
-    def _list_outputs(self):
-        """
-        The ANTS package implicitly outputs three files based on the prefix given at the commandline:
-        {prefix}Affine.txt, {prefix}_{index}Warp.nii.gz, and {prefix}_{index}InverseWarp.nii.gz.  This function exposes
-        them for subsequent nodes.
-        """
-        from os.path import abspath
-        outputs = self.output_spec().get()
-        #outputs['output_affine'] = abspath( self.inputs.output_prefix + 'Affine.txt')
-        outputs['output_affine'] = abspath( self.inputs.initialTransform )
-        outputs['output_warp'] = abspath( self.inputs.output_prefix + '1' + 'Warp.nii.gz')
-        outputs['output_inversewarp'] = abspath( self.inputs.output_prefix + '1' + 'InverseWarp.nii.gz')
-        return outputs
 
 if __name__ == '__main__':
     RF12Test = RF12BRAINSCutWrapper(sys.argv)
