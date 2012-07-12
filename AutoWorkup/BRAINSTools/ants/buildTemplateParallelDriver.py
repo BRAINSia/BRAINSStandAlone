2d4672d8dd5e93a8a9b746f333cfce0e788a927c
Jessica Forbes <jessica-forbes@uiowa.edu>
2012-07-12 10:13:18 -0500

    ENH: Added argparse and cleaned code.

--- AutoWorkup/BRAINSTools/ants/buildTemplateParallelDriver.py
+++ AutoWorkup/BRAINSTools/ants/buildTemplateParallelDriver.py
@@ -3,58 +3,55 @@ import nipype.pipeline.engine as pe
 import argparse
 import nipype.interfaces.utility as util
 
-ExperimentBaseDirectoryCache = '/scratch/antsbuildtemplate/TEST_CACHE4'
+def BuildTemplateParallelWorkFlow():
 
-btp = pe.Workflow( name='buildtemplateparallel')
-btp.config['execution'] = {
-                                     'plugin':'Linear',
-                                     #'stop_on_first_crash':'true',
-                                     #'stop_on_first_rerun': 'true',
-                                     'stop_on_first_crash':'true',
-                                     'stop_on_first_rerun': 'false',      ## This stops at first attempt to rerun, before running, and before deleting previous results.
-                                     'hash_method': 'timestamp',
-                                     'single_thread_matlab':'true',       ## Multi-core 2011a  multi-core for matrix multiplication.
-                                     'remove_unnecessary_outputs':'false',
-                                     'use_relative_paths':'false',         ## relative paths should be on, require hash update when changed.
-                                     'remove_node_directories':'false',   ## Experimental
-                                     'local_hash_check':'true',           ##
-                                     'job_finished_timeout':15            ##
-                                     }
-btp.config['logging'] = {
-      'workflow_level':'DEBUG',
-      'filemanip_level':'DEBUG',
-      'interface_level':'DEBUG',
-      'log_directory': ExperimentBaseDirectoryCache
-    }
-btp.base_dir = ExperimentBaseDirectoryCache
+    ExperimentBaseDirectoryCache = '/scratch/antsbuildtemplate/TEST_CACHE4'
 
-imagedir = '/hjohnson/HDNI/ANTS_TEMPLATE_BUILD/run_dir/'
-images = ['{0}01_T1_half.nii.gz'.format(imagedir), '{0}02_T1_half.nii.gz'.format(imagedir),'{0}03_T1_half.nii.gz'.format(imagedir)]
+    btp = pe.Workflow( name='buildtemplateparallel')
+    btp.config['execution'] = {
+                                         'plugin':'Linear',
+                                         #'stop_on_first_crash':'true',
+                                         #'stop_on_first_rerun': 'true',
+                                         'stop_on_first_crash':'true',
+                                         'stop_on_first_rerun': 'false',      ## This stops at first attempt to rerun, before running, and before deleting previous results.
+                                         'hash_method': 'timestamp',
+                                         'single_thread_matlab':'true',       ## Multi-core 2011a  multi-core for matrix multiplication.
+                                         'remove_unnecessary_outputs':'false',
+                                         'use_relative_paths':'false',         ## relative paths should be on, require hash update when changed.
+                                         'remove_node_directories':'false',   ## Experimental
+                                         'local_hash_check':'true',           ##
+                                         'job_finished_timeout':15            ##
+                                         }
+    btp.config['logging'] = {
+          'workflow_level':'DEBUG',
+          'filemanip_level':'DEBUG',
+          'interface_level':'DEBUG',
+          'log_directory': ExperimentBaseDirectoryCache
+        }
+    btp.base_dir = ExperimentBaseDirectoryCache
 
-myInitAvgWF = initAvgWF()
-myMainWF = mainWF()
-infosource = pe.Node(interface=util.IdentityInterface(fields=['images']), name='infoSource' )
-infosource.inputs.images = images
+    myInitAvgWF = initAvgWF(ExperimentBaseDirectoryCache)
+    myMainWF = mainWF(ExperimentBaseDirectoryCache)
+    infosource = pe.Node(interface=util.IdentityInterface(fields=['images']), name='infoSource' )
+    infosource.inputs.images = args.inputVolumes
 
-btp.connect(infosource, 'images', myInitAvgWF, 'InputSpec.images')
-btp.connect(infosource, 'images', myMainWF, 'InputSpec.images')
-btp.connect(myInitAvgWF, 'OutputSpec.average_image', myMainWF, 'InputSpec.fixed_image')
+    btp.connect(infosource, 'images', myInitAvgWF, 'InputSpec.images')
+    btp.connect(infosource, 'images', myMainWF, 'InputSpec.images')
+    btp.connect(myInitAvgWF, 'OutputSpec.average_image', myMainWF, 'InputSpec.fixed_image')
 
-secondRun = myMainWF.clone(name='secondRun')
-btp.connect(infosource, 'images', secondRun, 'InputSpec.images')
-btp.connect(myMainWF, 'OutputSpec.template', secondRun, 'InputSpec.fixed_image')
+    secondRun = myMainWF.clone(name='secondRun')
+    btp.connect(infosource, 'images', secondRun, 'InputSpec.images')
+    btp.connect(myMainWF, 'OutputSpec.template', secondRun, 'InputSpec.fixed_image')
 
-btp.write_graph(graph2use='hierarchical')
-btp.write_graph(graph2use='exec')
+    #btp.run(plugin='MultiProc', plugin_args={'n_procs' : 3})
 
-btp.run(plugin='MultiProc', plugin_args={'n_procs' : 3})
+    btp.write_graph(graph2use='hierarchical')
+    #btp.write_graph(graph2use='exec')
 
-
-#if __name__ == "__main__":
-#    # Create and parse input arguments
-#    parser = argparse.ArgumentParser(description='')
-#    #parser.add_argument('-o', '--outputVolume', dest='outputVolume', help='The ANTS template output volume.')
-#    parser.add_argument('-i', '--inputVolumes', nargs='*', dest='inputVolumes', help='The ANTS template input volumes.')
-#    args = parser.parse_args()
-#    #print args.outputVolume
-#    BuildTemplateParallel()
+if __name__ == "__main__":
+    # Create and parse input arguments
+    parser = argparse.ArgumentParser(description='')
+    #parser.add_argument('-o', '--outputVolume', dest='outputVolume', help='The ANTS template output volume.')
+    parser.add_argument('-i', '--inputVolumes', nargs='*', dest='inputVolumes', help='The ANTS template input volumes.')
+    args = parser.parse_args()
+    BuildTemplateParallelWorkFlow()
