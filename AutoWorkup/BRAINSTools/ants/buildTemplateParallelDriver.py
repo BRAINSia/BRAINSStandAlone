1268f1deaee61fb150d3f7c11eb321efe1fe7190
Hans Johnson <hans-johnson@uiowa.edu>
2012-07-12 15:14:56 -0500

    ENH: Add multiple dispatch options.

--- AutoWorkup/BRAINSTools/ants/buildTemplateParallelDriver.py
+++ AutoWorkup/BRAINSTools/ants/buildTemplateParallelDriver.py
@@ -4,7 +4,7 @@ import argparse
 import nipype.interfaces.utility as util
 import textwrap
 
-def BuildTemplateParallelWorkFlow():
+def BuildTemplateParallelWorkFlow(input_arguments):
 
     ExperimentBaseDirectoryCache = '/scratch/antsbuildtemplate/TEST_CACHE4'
 
@@ -34,7 +34,7 @@ def BuildTemplateParallelWorkFlow():
     myInitAvgWF = initAvgWF(ExperimentBaseDirectoryCache)
     myMainWF = mainWF(ExperimentBaseDirectoryCache)
     infosource = pe.Node(interface=util.IdentityInterface(fields=['images']), name='infoSource' )
-    infosource.inputs.images = args.inputVolumes
+    infosource.inputs.images = input_arguments.inputVolumes
 
     btp.connect(infosource, 'images', myInitAvgWF, 'InputSpec.images')
     btp.connect(infosource, 'images', myMainWF, 'InputSpec.images')
@@ -44,10 +44,42 @@ def BuildTemplateParallelWorkFlow():
     btp.connect(infosource, 'images', secondRun, 'InputSpec.images')
     btp.connect(myMainWF, 'OutputSpec.template', secondRun, 'InputSpec.fixed_image')
 
-    btp.run(plugin='MultiProc', plugin_args={'n_procs' : 3})
-
     btp.write_graph(graph2use='hierarchical')
     #btp.write_graph(graph2use='exec')
+
+    SGEFlavor='SGE'
+    if input_arguments.wfrun == 'helium_all.q':
+        btp.run(plugin=SGEFlavor,
+            plugin_args=dict(template=JOB_SCRIPT,qsub_args="-S /bin/bash -pe smp1 1-4 -l mem_free=4000M -o /dev/null -e /dev/null "+CLUSTER_QUEUE))
+    if input_arguments.wfrun == 'helium_all.q_graph':
+        SGEFlavor='SGEGraph' #Use the SGEGraph processing
+        btp.run(plugin=SGEFlavor,
+            plugin_args=dict(template=JOB_SCRIPT,qsub_args="-S /bin/bash -pe smp1 1-4 -l mem_free=4000M -o /dev/null -e /dev/null "+CLUSTER_QUEUE))
+    elif input_arguments.wfrun == 'ipl_OSX':
+        btp.write_graph()
+        print "Running On ipl_OSX"
+        btp.run(plugin=SGEFlavor,
+            plugin_args=dict(template=JOB_SCRIPT,qsub_args="-S /bin/bash -pe smp1 1-4 -l mem_free=4000M -o /dev/null -e /dev/null "+CLUSTER_QUEUE))
+    elif input_arguments.wfrun == 'local_4':
+        btp.write_graph()
+        print "Running with 4 parallel processes on local machine"
+        btp.run(plugin='MultiProc', plugin_args={'n_procs' : 4})
+    elif input_arguments.wfrun == 'local_12':
+        btp.write_graph()
+        print "Running with 12 parallel processes on local machine"
+        btp.run(plugin='MultiProc', plugin_args={'n_procs' : 12})
+    elif input_arguments.wfrun == 'local':
+        try:
+            btp.write_graph()
+        except:
+            pass
+        print "Running sequentially on local machine"
+        btp.run()
+    else:
+        print "You must specify the run environment type. [helium_all.q,ipl_OSX,local_4,local_12,local]"
+        print input_arguments.wfrun
+        sys.exit(-1)
+
 
 if __name__ == "__main__":
     # Create and parse input arguments
@@ -57,7 +89,8 @@ if __name__ == "__main__":
   the ANTS build template parallel scheme. \n
 
 Common Usage:
-  $ python buildTemplateParallelDriver.py -i
+  $ python buildTemplateParallelDriver.py -wfrun local_4
+  -i
   /hjohnson/HDNI/ANTS_TEMPLATE_BUILD/run_dir/01_T1_half.nii.gz
   /hjohnson/HDNI/ANTS_TEMPLATE_BUILD/run_dir/02_T1_half.nii.gz
   /hjohnson/HDNI/ANTS_TEMPLATE_BUILD/run_dir/03_T1_half.nii.gz
@@ -65,5 +98,7 @@ Common Usage:
 """))
     #parser.add_argument('-o', '--outputVolume', dest='outputVolume', help='The ANTS template output volume.')
     parser.add_argument('-i', '--inputVolumes', nargs='*', dest='inputVolumes', help='The ANTS template input volumes.')
-    args = parser.parse_args()
-    BuildTemplateParallelWorkFlow()
+    parser.add_argument('-wfrun', action="store", dest='wfrun', required=True,
+                       help='The name of the workflow running plugin to use')
+    input_arguments = parser.parse_args()
+    BuildTemplateParallelWorkFlow(input_arguments)
