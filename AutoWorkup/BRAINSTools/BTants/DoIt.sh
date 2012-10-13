022ee3b4d5c1b6728b069f621e06fd50026067d0
Hans Johnson <hans-johnson@uiowa.edu>
2012-10-13 16:49:28 -0500

    ENH: Start of the TrackHD customizations.

--- AutoWorkup/BRAINSTools/BTants/DoIt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HERE=$(pwd)
-
-time python /IPLlinux/raid0/homes/jforbes/git/BRAINSStandAlone/AutoWorkup/BRAINSTools/BTants/btp_exp.py \
-   -ExperimentConfig ${1} \
-   -pe OSX_ENVIRONMENT \
-   -wfrun ${2}
-#-wfrun local
