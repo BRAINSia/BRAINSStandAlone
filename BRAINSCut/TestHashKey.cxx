cac39f0b0a7e7ca13c8c61eb21ade01f59587fe0
Regina Kim <eunyoung.regina.kim@gmail.com>
2012-12-03 14:15:15 -0600

    ADD: Normalizatio option and nipype for BCut
    ENH: Normalization Parameter
    
    Conflicts:
    
    	BRAINSCut/FeatureInputVector.cxx
    	BRAINSCut/FeatureInputVector.h
    
    Bug Fix
    
    DEV: Nipype for BCut cross validation
    
    DEV: Working version until train
    
    Dev: Working version of crossValidat(Unit).py
    
    Dev: Working version of crossValidat(Unit).py
    
    Adjustment for Cluster
    
    DEL unnecessary files
    
    Modification to deal with iterables more wisely
    
    del test files
    
    Working except for DataSink
    
    ADD Working DataSink/ Independent Shuffleing from Training
    
    Add substitution of DataSink
    
    ADD: LabelStat Pipeline
    
    ENH: Normalization Parameter
    
    Conflicts:
    
    	BRAINSCut/FeatureInputVector.cxx
    	BRAINSCut/FeatureInputVector.h
    
    Bug Fix
    
    DEV: Nipype for BCut cross validation
    
    DEV: Working version until train
    
    Dev: Working version of crossValidat(Unit).py
    
    Dev: Working version of crossValidat(Unit).py
    
    Adjustment for Cluster
    
    Add and Test: Normalizations and Unify two min/max definition
    
    Minor bug after merge
    
    Add minor
    
    Min/Max value fix
    
    Critical Bug Fix: Default value of feature creation fix and nipype
    normalization input fix
    
    nipype bug fix

--- BRAINSCut/TestHashKey.cxx
+++ BRAINSCut/TestHashKey.cxx
@@ -4,5 +4,12 @@ int
 main(int /*argc*/, char * [] /*argv*/)
 {
   FeatureInputVector testFeatureInputVector;
+
+  bool allTestPass = true;
+  if ( ! testFeatureInputVector.DoScalingUnitTests() )
+  {
+    allTestPass = false;
+  }
+
   return testFeatureInputVector.DoUnitTests();
 }
