6b89fb01697158788fb3e8b60e470f8c06d2e9f3
Hans Johnson <hans-johnson@uiowa.edu>
2013-01-28 14:38:07 -0600

    BUG: Error handeling when no transform given.
    When no transform type is given, a better error message must be created.
    The exception handeling of this case was updated.

--- BRAINSCommonLib/BRAINSFitHelperTemplate.hxx
+++ BRAINSCommonLib/BRAINSFitHelperTemplate.hxx
@@ -36,6 +36,10 @@ ValidateTransformRankOrdering(const std::vector<std::string> & transformType)
                            << "smallest to largest.  Please review settings for transformType.\n" \
                            << "Rigid < ScaleVersor3D < ScaleSkewVersor3D < Affine < (BSpline | SyN)")
 
+  if ( transformType.empty() )
+    {
+    itkGenericExceptionMacro(<< " ERROR:  No transform type specified.");
+    }
   unsigned int CurrentTransformRank = 0;
   for( unsigned int l = 0; l < transformType.size(); ++l )
     {
